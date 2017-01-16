06098a248e7e92437d26e0e939bbca24aeccc8c7
MikaelFeher <mikael.feher@gmail.com>
2017-01-16 23:11:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Weather Scraper
+
+A PHP app to check the weather, just enter the city name and the app scrapes the information from [Weather-Forecast.com](http://www.weather-forecast.com/).
+
+This project is created as a PHP-learning experience during [The Complete Web Developer Course 2.0](https://www.udemy.com/the-complete-web-developer-course-2/).
+
+A live version of the app can be found [here]().
+
+**DISCLAIMER!**
+
+[Weather-Forecast.com](http://www.weather-forecast.com/) is in now way associated with this project and the app only works as long as [Weather-Forecast.com](http://www.weather-forecast.com/) doesn't mind the traffic.
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+	$output = '';
+
+
+	if ($_GET['city']) {
+
+		$input = explode(' ', $_GET['city']);
+		$city = implode('-', $input);
+
+		$forecastPage = file_get_contents('http://www.weather-forecast.com/locations/'.$city.'/forecasts/latest');
+
+		$firstDelimiter = '3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">';
+
+		$secondDelimiter = '</span></span></span></p><div class="forecast-cont"><div class="units-cont"><a class="units metric active">&deg;C</a><a class="units imperial">&deg;F</a></div>';
+
+		$strOne = explode($firstDelimiter, $forecastPage);
+
+		$strTwo = explode($secondDelimiter, $strOne[1]);
+		$forecast = $strTwo[0];
+
+		if ($forecast) {
+			$output = '<div class="alert alert-success">'.$forecast.'</div>';
+		} else {
+			$output = '<div class="alert alert-danger"><strong>Oops, something went wrong :(</strong><br>If your spelling is correct then the city isn\'t in the system. </div>';
+		}
+
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+	<style rel="stylesheet" type="text/css" media="screen">
+		body{
+			padding: 0;
+			margin: 0;
+			height:100vh;
+			background-image: url('bg2.jpg');
+			background-repeat: no-repeat;
+			background-size:cover;
+		}
+		.jumbotron {
+			text-align: center;
+			padding-top: 100px;
+			background: transparent;
+		}
+		form {
+			margin-bottom: 40px;
+		}
+	</style>
+  </head>
+  <body>
+
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 offset-sm-2">
+					<h1 class="display-4">What's The Weather?</h1>
+					<p class="lead pb-3">Enter the name of a city. <br> <span><small>(English names only...)</small></span></p>
+
+					<form>
+						<input type="text" class="form-control form-control-lg col-xs-6 offset-xs-3 my-3" name="city" placeholder="Eg. London, Malmo" value="<?php echo $city; ?>">
+
+						<button type="submit" class="btn btn-primary btn-lg mt-4">Submit</button>
+
+					</form>
+
+					<div id="message">
+						<?php echo $output; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	
+  </body>
+</html>
